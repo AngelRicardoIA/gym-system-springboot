a184e17739ba5c5d0c2afb5118db74076c70960a
AngelIA <angel.aceves1726@gmail.com>
2026-05-09 00:02:23 -0600

    Commit inicial

---
+++ src/test/java/com/angel/GymSystem/GymSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.GymSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
